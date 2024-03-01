202e5ee37300006cd13a6bd5612f384c3a68e523
EricHuynh10 <eric.cd.huynh@outlook.com>
2024-03-02 10:48:46 +1100

    included API into SummaryEarn component

--- Frontend/src/components/SummaryEarn.jsx
+++ Frontend/src/components/SummaryEarn.jsx
@@ -1,17 +1,45 @@
-import React, { useState, useEffect  } from 'react';
+import React, { useState, useEffect } from 'react';
 import "../styles.css";
 import axios from 'axios';
 
-class SummaryEarn extends React.Component {
-    render() {
-        return (
-            <div>
-                <h2>Latest warning</h2>
-                
-            
-            </div>
-        )
+const SummaryEarn = () => {
+    const [warning, setWarning] = useState(null);
+    // const [ticker, setTicker] = useState('');
+    // const [threshold, setThreshold] = useState('');
 
+    let ticker = 'GOOG';
+    let threshold = 0.001;
+
+    useEffect(() => {
+        fetchWarning(ticker, threshold);
+    }, []);
+
+    const fetchWarning = async (ticker, threshold) => {
+        try {
+            let url = `${process.env.REACT_APP_BACKEND_URL}/warning?ticker=${ticker}&threshold=${threshold}`;
+            const response = await axios.get(url);
+            setWarning(response.data);
+            console.log('response: ', response.data);
+            console.log('warning: ', warning);
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching warning: ', error);
+        }
     }
+    
+
+    return (
+        <div class="warning">
+            <h2>Latest warning</h2>
+            {warning && (
+                <div>
+                    <h3>{ticker}</h3>
+                    <p>Prediction: {warning.Prediction ? `${(warning.Prediction * 100).toFixed(1)}%` : '-'}</p>
+                    <p>{warning.Summary}</p>
+                </div>
+            )}
+        </div>
+    )
 }
+
 export default SummaryEarn;
