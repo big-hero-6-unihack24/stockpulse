4d3adc88df693b2ab68719d75803d0286b4f0e43
May Thu Nguyen <104744232+Maythunguyen@users.noreply.github.com>
2024-03-03 00:34:13 +1100

    Update TrackingForm.jsx

--- Frontend/src/components/TrackingForm.jsx
+++ Frontend/src/components/TrackingForm.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import { BsApp } from "react-icons/bs";
 import { HiOutlineArrowSmDown } from "react-icons/hi";
 import { TfiTrash } from "react-icons/tfi";
-import { FcGoogle } from "react-icons/fc";
-import { BsApple } from "react-icons/bs";
 
 import { LuPen } from "react-icons/lu";
 import "../styles.css";
 
 class TrackingForm extends React.Component {
     render() {
+        const { selectedTickers } = this.props;
         return (
             <div>
                 <table className="table-container">
@@ -18,26 +17,19 @@ class TrackingForm extends React.Component {
                         <th>Name</th>
                         <th>Symbol <HiOutlineArrowSmDown /></th>
                     </tr>
-                    <tr>
+                    {selectedTickers.map((ticker, index) => (
+                        <tr key={index}>
                         <td><BsApp /></td>
                         <td className="td-name">
-                            <div><FcGoogle  size={16}/> </div>
-                            <div>Google</div>
+                            <div>{ticker.icon} </div>
+                            <div>{ticker.label}</div>
                         </td>
-                        <td className="symbol">GOOG</td>
+                        <td className="symbol">{ticker.value}</td>
                         <td><TfiTrash /></td>
-                        <td><LuPen /></td>
-                    </tr>
-                    <tr>
-                        <td><BsApp /></td>
-                        <td className="td-name">
-                            <div><BsApple size={16}/> </div>
-                            <div>Apple</div>
-                        </td>
-                        <td className="symbol">APPL</td>
-                        <td><TfiTrash /></td>
-                        <td><LuPen /></td>
-                    </tr>
+                        <td><LuPen/></td>
+
+                        </tr>
+                    ))}
   
                 </table>
             </div>
