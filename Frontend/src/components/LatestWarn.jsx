1d1a24e568cf960c93e1de4794045412eb4909ac
May Thu Nguyen <104744232+Maythunguyen@users.noreply.github.com>
2024-03-07 12:44:17 +1100

    Update LatestWarn.jsx

--- Frontend/src/components/LatestWarn.jsx
+++ Frontend/src/components/LatestWarn.jsx
@@ -1,61 +1,59 @@
 import React, { useState, useEffect } from 'react';
 import "../styles.css";
 import axios from 'axios';
-import { AiOutlineArrowUp } from "react-icons/ai";
-import { AiOutlineArrowDown } from "react-icons/ai";
-const LatestWarn = () => {
-    const [warning, setWarning] = useState(null);
-    const [warningDate, setWarningDate] = useState('');
+import { AiOutlineArrowUp, AiOutlineArrowDown } from "react-icons/ai";
 
-    const ticker = 'META'; 
+const LatestWarn = ({ selectedTickers }) => {
+    const [warnings, setWarnings] = useState({});
+    const [warningDates, setWarningDates] = useState({});
+
     const threshold = 0.001;
 
     useEffect(() => {
-        const fetchWarning = async () => {
-            try {
-                const url = `${process.env.REACT_APP_BACKEND_URL}/warning?ticker=${ticker}&threshold=${threshold}`;
-                const response = await axios.get(url);
-                if (response.data) {
-                    setWarning(response.data);
-                    setWarningDate(new Date().toLocaleDateString());
+        selectedTickers.forEach(ticker => {
+            const fetchWarning = async () => {
+                try {
+                    const url = `${process.env.REACT_APP_BACKEND_URL}/warning?ticker=${ticker.value}&threshold=${threshold}`;
+                    const response = await axios.get(url);
+                    if (response.data) {
+                        setWarnings(prevWarnings => ({ ...prevWarnings, [ticker.value]: response.data }));
+                        setWarningDates(prevDates => ({ ...prevDates, [ticker.value]: new Date().toLocaleDateString() }));
+                    }
+                } catch (error) {
+                    console.error('Error fetching warning: ', error);
                 }
-            } catch (error) {
-                console.error('Error fetching warning: ', error);
-            }
-        };
-        fetchWarning();
-    }, [ticker, threshold]); 
-
-    const predictionValue = warning && warning.Prediction ? (warning.Prediction * 100).toFixed(1) : 'N/A';
-
-    const renderPredictionIcon = (prediction) => {
-        if (prediction > 0) {
-            return  <AiOutlineArrowUp className="prediction-positive" />;
-        } else {
-            return <AiOutlineArrowDown className="prediction-negative" />;
-        } 
-    };
+            };
+            fetchWarning();
+        });
+    }, [selectedTickers]);
 
     return (
-        <div className="warning">
-            <div className='warning-day'>
-                <div><h3>Latest warning</h3></div>
-                <div><p>{warningDate}</p></div>
-            </div>
-        
-            {warning && (
-                <div className="warning-infor">
-                    <div className="warning-ticker"><h3>{ticker}</h3></div>
-                        {/* <p>Potential {warning.Prediction ? `${(warning.Prediction * 100).toFixed(1)}%` : 'N/A'} </p> */}
-                       <div className="warning-potential"><p className="prediction-value">Potential {predictionValue}% {renderPredictionIcon(warning.Prediction)}</p></div>
-
-                    <div className="warning-sum">
-                        <div><h3>Summary Earnings </h3></div>
-                        <div><p>{warning.Summary}</p></div>
-                    </div>
-                </div>
-            )}
-        </div>
+    
+            <table className='warning-container'>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Ticker</th>
+                        <th>Prediction</th>
+                        <th>Summary Earnings</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {selectedTickers.map(ticker => (
+                        warnings[ticker.value] ? (
+                            <tr key={ticker.value}>
+                                <td>{warningDates[ticker.value]}</td>
+                                <td className='warning-ticker'>{ticker.label}</td>
+                                <td>
+                                    Potential {warnings[ticker.value].Prediction ? `${(warnings[ticker.value].Prediction * 100).toFixed(1)}%` : 'N/A'} 
+                                    {warnings[ticker.value].Prediction > 0 ? <AiOutlineArrowUp className="prediction-positive" /> : <AiOutlineArrowDown className="prediction-negative" />}
+                                </td>
+                                <td className='summary-earnings'>{warnings[ticker.value].Summary}</td>
+                            </tr>
+                        ) : null
+                    ))}
+                </tbody>
+            </table>
     );
 };
 
